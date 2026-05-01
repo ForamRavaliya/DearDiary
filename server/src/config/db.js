ea8849a41a23e0b8dedd5299bad1b713cb36e260
ForamRavaliya <fravaliya2107@gmail.com>
2026-05-01 16:45:20 +0530

    Update project

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,11 +3,9 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
-// Test DB connection (optional but useful)
-pool.connect()
-  .then(() => console.log("PostgreSQL Connected"))
-  .catch(err => console.error("DB Connection Error:", err));
 
 module.exports = pool;
